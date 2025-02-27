88e553c5722a304646cd4b3355e95aa60934c3e2
satendrcoder <satendracaria@gmail.com>
2025-02-27 17:09:20 +0530

    CMD Voice Tools is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,11 @@ import { MateriallistModule } from './shared/materiallist/materiallist.module';
 export class AppComponent {
   title = 'chat-bot mujhe nhi '
 
+  categories = [
+    { name: 'Shopping', icon: 'bi bi-bag', color: '#d6e4ff', iconBgColor: '#91b5f2' },
+    { name: 'Entertainment', icon: 'bi bi-film', color: '#e4e4e4', iconBgColor: '#bdbdbd' },
+    { name: 'Tools', icon: 'bi bi-tools', color: '#bfe3ff', iconBgColor: '#63a8e1' },
+    { name: 'Art & Design', icon: 'bi bi-pencil', color: '#c6efce', iconBgColor: '#69c370' }
+  ];
 }
 
